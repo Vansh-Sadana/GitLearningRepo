413a2234d916d8ac2bf54d2a013989fe31c65701
unknown <vanshsadana9822@gmail.com>
2021-03-28 14:59:28 +0530

    save cjanges in singeleton

---
+++ singleton.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+class singleton
+{
+    private singleton()
+    {
+        
+    }
+    public static singleton g = null;
+    public static singleton getInstance()
+    {
+        if(g == null)
+        {
+            g = new singleton();
+        }
+        return g;
+    }
+}
+class second
+{
+    public static void main(String args[])
+    {
+        singleton j = singleton.getInstance();
+        System.out.println("changing the code with git");
+    }
+}
